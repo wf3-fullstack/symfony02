05a845dd8aaaefef9b3a56af9059d1b90509ccfb
laurentFreundWF3 <laurent.freund@wf3.fr>
2020-01-03 17:03:32 +0100

    rajout de array_unique pour getRoles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -70,7 +70,8 @@ class User implements UserInterface
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
-        return $roles;
+        // ENLEVE LES DOUBLONS 'ROLE_USER'
+        return array_unique($roles);
     }
 
     public function setRoles($roles): self
